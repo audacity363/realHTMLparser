1b9627e73178e3f9348e9f29c0ec4f73a3a3c21e
Tom Engemann <tom@engemann.me>
2016-12-18 15:06:35 +0100

    Added error handling to the JSON function

--- json.c
+++ json.c
@@ -13,11 +13,14 @@ int exec_json(token_t *anker, status_t *stat)
     memset(name_buff, 0x00, sizeof(name_buff));
 
     int index_name = 0,
-        var_count = 0;
+        var_count = 0, i = 0;
     
     token_t *hptr = NULL;
 
-    varnames = malloc(sizeof(char*));
+    if(!(varnames = malloc(sizeof(char*))))
+    {
+        return(-1);
+    }
 
     hptr = anker;
 
@@ -26,11 +29,20 @@ int exec_json(token_t *anker, status_t *stat)
         if(hptr->type == COMMA || hptr->type == CLAMPS)
         {
             memset(name_buff+index_name, 0x00, sizeof(wchar_t));
-            varnames[var_count] = malloc(sizeof(char)*wcslen(name_buff));
+            if(!(varnames[var_count] = malloc(sizeof(char)*wcslen(name_buff))))
+            {
+                for(i=0; i < var_count; i++)
+                    free(varnames[i]);
+                free(varnames);
+                return(-2)
+            }
 
             wcstombs(varnames[var_count], name_buff, wcslen(name_buff)+1);
 
-            varnames = realloc(varnames, sizeof(char*)*((++var_count)+1));
+            if(!(varnames = realloc(varnames, sizeof(char*)*((++var_count)+1))))
+            {
+                return(-3);
+            }
 
             index_name = 0;
         }
@@ -42,8 +54,18 @@ int exec_json(token_t *anker, status_t *stat)
         hptr = hptr->next;
     }
 
-    //printAllVarsJSON(vars_anker);
     if(printVarsToFileJSON(vars_anker, varnames, var_count, stdout) != 0)
-        printf("Kenn ich nicht\n");
+    {
+        //variable not known
+        for(i=0; i < var_count; i++)
+            free(varnames[i]);
+        free(varnames);
+        return(-4);
+    }
+
+    for(i=0; i < var_count; i++)
+        free(varnames[i]);
+    free(varnames);
+
     return(0);
 }
